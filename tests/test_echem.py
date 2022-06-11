d72ea713c008d7080eeb040754306282e7e1f7ae
Matthew Evans <git@ml-evs.science>
2022-06-11 12:47:30 +0100

    Add simple tests and MPR example data

---
+++ tests/test_echem.py
@@ -0,0 +1,115 @@
+import pathlib
+import unittest
+
+import pytest
+
+
+def test_xlsx_reader_and_dqdv():
+    """Run through the example notebook as a test."""
+    import navani.echem as ec
+    import numpy as np
+    import pandas as pd
+
+    test_path = pathlib.Path(__file__).parent.joinpath(
+        "../Example_data/bs542_004_gr_li_50ua_50mv_1v_191020_Channel_11.xlsx"
+    )
+    df = ec.echem_file_loader(test_path)
+    assert df.shape == (4526, 20)
+
+    cols = (
+        "index",
+        "Date_Time",
+        "Test_Time(s)",
+        "Step_Time(s)",
+        "Step_Index",
+        "Cycle_Index",
+        "Voltage(V)",
+        "Current(A)",
+        "Charge_Capacity(Ah)",
+        "Discharge_Capacity(Ah)",
+        "Charge_Energy(Wh)",
+        "Discharge_Energy(Wh)",
+        "Internal Resistance(Ohm)",
+        "dV/dt(V/s)",
+        "state",
+        "cycle change",
+        "half cycle",
+        "Capacity",
+        "Voltage",
+        "full cycle",
+    )
+
+    assert set(cols) == set(df)
+
+    mask = df["half cycle"] == 1
+    voltage, dqdv, capacity = ec.dqdv_single_cycle(
+        df["Capacity"][mask],
+        df["Voltage"][mask],
+        window_size_1=51,
+        polyorder_1=5,
+        s_spline=0.0,
+        window_size_2=51,
+        polyorder_2=5,
+        final_smooth=True,
+    )
+
+    assert voltage.shape == (10000,)
+    assert dqdv.shape == (10000,)
+    assert capacity.shape == (10000,)
+
+    np.testing.assert_almost_equal(np.mean(voltage), 1.33431, decimal=3)
+    np.testing.assert_almost_equal(np.mean(capacity), 0.0, decimal=3)
+    np.testing.assert_almost_equal(np.mean(dqdv), -0.002, decimal=3)
+
+
+def test_mpr_reader():
+    import navani.echem as ec
+    import numpy as np
+    import pandas as pd
+
+    test_path = pathlib.Path(__file__).parent.joinpath(
+        "../Example_data/jdb11-1_c3_gcpl_5cycles_2V-3p8V_C-24_data_C09.mpr"
+    )
+    df = ec.echem_file_loader(test_path)
+    assert df.shape == (46102, 16)
+
+    cols = (
+        "state",
+        "Capacity",
+        "dQ/mA.h",
+        "Voltage",
+        "Q charge/discharge/mA.h",
+        "(Q-Qo)/mA.h",
+        "time/s",
+        "Ns",
+        "dt",
+        "half cycle",
+        "P/W",
+        "full cycle",
+        "Current",
+        "control/V/mA",
+        "I Range",
+        "flags",
+    )
+
+    assert set(cols) == set(df)
+
+    mask = df["half cycle"] == 1
+    voltage, dqdv, capacity = ec.dqdv_single_cycle(
+        df["Capacity"][mask],
+        df["Voltage"][mask],
+        window_size_1=51,
+        polyorder_1=5,
+        s_spline=0.0,
+        window_size_2=51,
+        polyorder_2=5,
+        final_smooth=True,
+    )
+
+    assert voltage.shape == (10000,)
+    assert dqdv.shape == (10000,)
+    assert capacity.shape == (10000,)
+
+    np.testing.assert_almost_equal(np.mean(voltage), 2.2544, decimal=3)
+    np.testing.assert_almost_equal(np.mean(capacity), 0.1202, decimal=3)
+    np.testing.assert_almost_equal(np.mean(dqdv), -0.4057, decimal=3)
