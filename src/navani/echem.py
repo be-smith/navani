5935d698c0dad6c0a9177cb228718ad591ce3607
BenSmithGreyGroup <ben.ed.smith@mac.com>
2025-05-15 21:46:18 +0100

    Fixed errors in capacity where there is a rest at the end of a charge cycle - used to go negative. Now set to the final active capacity value

--- src/navani/echem.py
+++ src/navani/echem.py
@@ -326,12 +326,29 @@ def arbin_res(df: pd.DataFrame) -> pd.DataFrame:
         raise KeyError('Unable to find capacity columns, do not match Charge_Capacity or Charge_Capacity(Ah)')
 
     # Subtracting the initial capacity from each half cycle so it begins at zero
+    # This could probably be done better by integrating the current over time like all the others - but I like to keep the capacity recorded by the machine where possible
     for cycle in df['half cycle'].unique():
-        idx = df[(df['half cycle'] == cycle) & (df['state'] != 'R')].index
-        if len(idx) > 0:
-            cycle_idx = df[df['half cycle'] == cycle].index
-            initial_capacity = df.loc[idx[0], 'Capacity']
-            df.loc[cycle_idx, 'Capacity'] = df.loc[cycle_idx, 'Capacity'] - initial_capacity
+        cycle_df = df[df['half cycle'] == cycle]
+        non_rest_idx = cycle_df[cycle_df['state'] != 'R'].index
+        rest_idx = cycle_df[cycle_df['state'] == 'R'].index
+
+        if len(non_rest_idx) > 0:
+            first_active_idx = non_rest_idx[0]
+            last_active_idx = non_rest_idx[-1]
+            initial_capacity = df.loc[first_active_idx, 'Capacity']
+
+            # Subtract initial capacity from all non-rest points
+            df.loc[non_rest_idx, 'Capacity'] -= initial_capacity
+
+            # Handle rest rows before the first active point by setting capacity to 0
+            pre_rest_idx = rest_idx[rest_idx < first_active_idx]
+            df.loc[pre_rest_idx, 'Capacity'] = 0
+
+            # Handle rest rows after the last active point by setting capacity to the final capacity value
+            post_rest_idx = rest_idx[rest_idx > last_active_idx]
+            final_capacity = df.loc[last_active_idx, 'Capacity']
+            df.loc[post_rest_idx, 'Capacity'] = final_capacity
+
         else:
             pass
     
